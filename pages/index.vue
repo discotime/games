7a2c730eff0213f8009d9863a6ada3754df5fb70
discotime <34188176+discotime@users.noreply.github.com>
2021-09-17 15:19:53 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,340 @@
-<template>
-  <Tutorial/>
-</template>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+<title>ColorofLife@RaspberryPI2</title>
+<link href="style.css" title="style" rel="stylesheet" type="text/css" />
+<link id="clink" href="css/style-blue.css" title="style" rel="stylesheet" type="text/css" media="screen" />
+<script src="scripts/jquery.min.js" type="text/javascript"></script>
+<script src="scripts/jquery.masonry.min.js" type="text/javascript"></script>
+<script src="scripts/jquery.easing.1.3.js" type="text/javascript"></script>
+<script src="scripts/MetroJs.lt.js" type="text/javascript"></script>
+<script src="scripts/jquery.fancybox-1.3.4.js" type="text/javascript" charset="utf-8"></script>
+<script src="scripts/jquery.flexslider-min.js" type="text/javascript" charset="utf-8"></script>
+<script src="scripts/hoverintent.js" type="text/javascript" charset="utf-8"></script>
+<script src="scripts/organictabs.jquery.js" type="text/javascript" charset="utf-8"></script>
+<!--[if lt IE 9]>
+  <style type="text/css">
+  @import url("style-ie8.css");
+  </style>
+  <script src="scripts/css3-mediaqueries.js" type="text/javascript" charset="utf-8"></script>
+  <script>
+    document.createElement('header');
+    document.createElement('nav');
+    document.createElement('section');
+    document.createElement('article');
+    document.createElement('aside');
+    document.createElement('footer');
+    document.createElement('hgroup');
+    </script>
+<![endif]-->
+<script src="scripts/javascript.js" type="text/javascript"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+<body>
+<div id="bodypat">
+<section id="container">
+    <div id="colorchanger">
+    <a href="?theme=blue" class="cblue cbox" title="Blue Theme"><span class="blue"> </span></a>
+    <a href="?theme=red" class="cred cbox" title="Red Theme"><span class="red"> </span></a>
+    <a href="?theme=green" class="cgreen cbox" title="Green Theme"><span class="green"> </span></a>
+    <a href="?theme=magenta" class="cmagenta cbox" title="Magenta Theme"><span class="magenta"> </span></a>
+    <a href="?theme=purple" class="cpurple cbox" title="Purple Theme"><span class="purple"> </span></a>
+    <a href="?theme=teal" class="cteal cbox" title="Teal Theme"><span class="teal"> </span></a>
+    <a href="?theme=lime" class="clime cbox" title="Lime Theme"><span class="lime"> </span></a>
+    <a href="?theme=brown" class="cbrown cbox" title="Brown Theme"><span class="brown"> </span></a>
+    <a href="?theme=pink" class="cpink cbox" title="Pink Theme"><span class="pink"> </span></a>
+    <a href="?theme=mango" class="cmango cbox" title="Mango Theme"><span class="mango"> </span></a>
+    <h4>Theme Color Selector</h4>
+    </div>
+<!-- BEGIN HEADER -->
+<header class="clearfix">
+<!-- BEGIN LOGO -->
+<a id="headerlink" href="#" title="home"><img id="logo" src="images/logo.png" alt="logo" /><span id="sitename">ColorofLife</span></a>
+<!-- END LOGO -->
+
+<!-- BEGIN NAVIGATION -->
+<nav>
+<ul id="nav" class="clearfix">
+<!-- Menu Item 1 -->
+	<li class="current"><a href="index.html" title="Home"><span>Home</span></a></li>
+<!-- Menu Item 3 -->
+    <li><a href="/gallery/" title="Gallery"><span>Gallery</span></a></li>
+<!-- Menu Item 5 -->
+    <li><a href="about.html" title="About"><span>About</span></a></li>
+</ul>
+</nav>
+<!-- END NAVIGATION -->
+</header>
+<!-- END HEADER -->
+
+<!-- BEGIN MAIN PAGE CONTENT -->
+<section class="mainpage">
+	<!-- BEGIN TOGGLE CONTENT -->
+	<div class="toggle-button"></div>
+    
+ <!-- end .toggle-content -->
+    <!-- END TOGGLE CONTENT -->
+</section><!-- end #mainpage -->
+
+<!-- BEGIN TILE CONTENT -->
+<section id="content-mos" class="centered clearfix">
+<!-- Tile 1 -->
+<a href="#article-1" class="lightbox" rel="section">
+    <div class="tile large live" data-stops="0,25%,50%,75%,100%" data-speed="3000" data-delay="0" data-direction="horizontal" data-stack="true">
+        <div class="live-front">
+        	<img class="live-img" src="images/articles/article1_right.png" alt="Article 1" />
+        </div>
+        <div class="live-back">
+        	<img class="live-img" src="images/articles/article1_left.png" alt="Article 1" />
+        </div>
+        <span class="tile-date redtxt"><span class="date">31</span><span class="month">May</span></span>
+        <span class="tile-cat red">热爱生命</span>
+    </div>
+</a>
+	<!-- Lightbox Article Preview -->
+    <div class="tile-pre">
+    <article id="article-1" class="lb-article">
+    <div class="article-img">
+        <div class="flexslider postslide">
+        <ul class="slides">
+    	<li>
+        <img class="tile-pre-img" src="images/articles/article1.png" alt="Article 1" />
+        </li>
+        <li>
+        <img class="tile-pre-img" src="images/articles/article1a.png" alt="Article 1" />
+        </li>
+        </ul>
+        </div>
+    </div>
+    <div class="article-date redtxt"><span class="date">31</span><span class="month">May</span></div>
+    <span class="postcat redtxt">热爱生命</span>
+    <div class="lb-excerpt">
+        <p>我不去想是否能够成功<br>既然选择了远方  便只顾风雨兼程<br>我不去想能否赢得爱情<br>既然钟情于玫瑰  就勇敢地吐露真诚<br>我不去想身后会不会袭来寒风冷雨<br>既然目标是地平线  留给世界的只能是背影<br>我不去想未来是平坦还是泥泞<br>只要热爱生命  一切  都在意料之中</p>
+    </div>
+    </article>
+    </div>
+    
+<!-- Tile 2 -->
+<a href="#article-2" class="lightbox" rel="section">
+    <div class="tile medium live" data-stops="0,75%,100%" data-speed="750" data-delay="1500">
+    	<div class="live-front blue">
+        	<img class="live-img" src="images/articles/article2_1.png" alt="Article 2" />
+        </div>
+        <div class="live-back green">
+        	<img class="live-img" src="images/articles/article2_2.png" alt="Article 2" />
+        </div>
+    </div>
+</a>
+	<!-- Lightbox Article Preview -->
+    <div class="tile-pre">
+    <article id="article-2">
+    <div class="article-img">
+    	<div class="flexslider postslide">
+        <ul class="slides">
+        <li>
+    	<img class="tile-pre-img" src="images/articles/article2.png" alt="Article Two" />
+        </li>
+        <li>
+    	<img class="tile-pre-img" src="images/articles/article2a.png" alt="Article Two" />
+        </li>
+        </ul>
+        </div>
+    </div>
+    </article>
+    </div>
+    
+<!-- Tile 3 -->
+<a href="#portfolio-1" class="lightbox" rel="section">
+    <div class="tile small">
+    <img class="live-img" src="images/portfolio/item1_sq.png" alt="Project One" />
+    </div>
+</a>
+	<!-- Lightbox Article Preview -->
+    <div class="tile-pre">
+    <article id="portfolio-1">
+    <div class="portfolio-img">
+    <img class="tile-pre-img" src="images/portfolio/item1.png" alt="Project One" />
+    </div>
+    </article>
+    </div>
+
+<!-- Tile 4 -->
+<a href="#portfolio-2" class="lightbox" rel="section">
+    <div class="tile small">
+    <img class="live-img" src="images/portfolio/item2_sq.png" alt="Project Two" />
+    </div>
+</a>
+    <!-- Lightbox Article Preview -->
+    <div class="tile-pre">
+    <article id="portfolio-2">
+    <div class="portfolio-img">
+    <img class="tile-pre-img" src="images/portfolio/item2.png" alt="Project Two" />
+    </div>
+    </article>
+    </div>
+
+<!-- Tile 5 -->
+<a href="#portfolio-3" class="lightbox" rel="section">
+    <div class="tile small">
+    <img class="live-img" src="images/portfolio/item3_sq.png" alt="Project Three" />
+    </div>
+</a>
+    <!-- Lightbox Article Preview -->
+    <div class="tile-pre">
+    <article id="portfolio-3">
+    <div class="portfolio-img">
+    <img class="tile-pre-img" src="images/portfolio/item3.png" alt="Project Three" />
+    </div>
+    </article>
+    </div>
+    
+<!-- Tile 6 -->
+<a href="#portfolio-6" class="lightbox" rel="section">
+    <div class="tile small">
+    <img class="live-img" src="images/portfolio/item4_sq.png" alt="Project Four" />
+    </div>
+</a>
+    <!-- Lightbox Article Preview -->
+    <div class="tile-pre">
+    <article id="portfolio-6">
+    <div class="portfolio-img">
+    <img class="tile-pre-img" src="images/portfolio/item4.png" alt="Project Four" />
+    </div>
+    </article>
+    </div>
+
+
+<!-- Tile 7 -->
+<a href="#portfolio-5" class="lightbox" rel="section">
+    <div class="tile small">
+    <img class="live-img" src="images/portfolio/item5_sq.png" alt="Project Five" />
+    </div>
+</a>
+    <!-- Lightbox Article Preview -->
+    <div class="tile-pre">
+    <article id="portfolio-5">
+    <div class="portfolio-img">
+    <img class="tile-pre-img" src="images/portfolio/item5.png" alt="Project Five" />
+    </div>
+    </article>
+    </div>
+    
+<!-- Tile 8 -->
+<a href="#portfolio-4" class="lightbox" rel="section">
+    <div class="tile small">
+    <img class="live-img" src="images/portfolio/item4_sq.png" alt="Project Four" />
+    </div>
+</a>
+    <!-- Lightbox Article Preview -->
+    <div class="tile-pre">
+    <article id="portfolio-4">
+    <div class="portfolio-img">
+    <img class="tile-pre-img" src="images/portfolio/item4.png" alt="Project Four" />
+    </div>
+    </article>
+    </div>
+
+<!-- Tile 9 -->
+ <a href="#portfolio-9" class="lightbox" rel="section">
+    <div class="tile small">
+    <img class="live-img" src="images/portfolio/item6_sq.png" alt="Project Seven" />
+    </div>
+</a>
+	<!-- Lightbox Article Preview -->
+    <div class="tile-pre">
+    <article id="portfolio-9">
+    <div class="portfolio-img">
+    <img class="tile-pre-img" src="images/portfolio/item6.png" alt="Project Seven" />
+    </div>
+    </article>
+    </div>
+    
+    
+<!-- Tile 11 -->
+<a href="#portfolio-11" class="lightbox" rel="section">
+    <div class="tile small">
+    <img class="live-img" src="images/portfolio/item2_sq.png" alt="Project Six" />
+    </div>
+</a>
+	<!-- Lightbox Article Preview -->
+    <div class="tile-pre">
+    <article id="portfolio-11">
+    <div class="portfolio-img">
+    <img class="tile-pre-img" src="images/portfolio/item2.png" alt="Project Six" />
+    </div>
+    </article>
+    </div>
+    
+
+    
+<!-- Tile 13 -->
+<a href="#article-3" class="lightbox" rel="section">
+    <div class="tile medium">
+        	<img class="live-img" src="images/articles/article3_tile.png" alt="Article 3" />
+    </div>
+</a>
+	<!-- Lightbox Article Preview -->
+    <div class="tile-pre">
+    <article id="article-3" class="lb-article">
+    <div class="article-img">
+    	<img class="tile-pre-img" src="images/articles/article3.png" alt="Article Three" />
+    </div>
+    </article>
+    </div>
+    
+<!-- Tile 14 -->
+<a href="#portfolio-7" class="lightbox" rel="section">
+    <div class="tile small">
+    <img class="live-img" src="images/portfolio/item7_sq.png" alt="Project Seven" />
+    </div>
+</a>
+	<!-- Lightbox Article Preview -->
+    <div class="tile-pre">
+    <article id="portfolio-7">
+    <div class="portfolio-img">
+    <img class="tile-pre-img" src="images/portfolio/item7.png" alt="Project Seven" />
+    </div>
+    </article>
+    </div>
+     
+
+<!-- Tile 16 -->
+<a href="#portfolio-8" class="lightbox" rel="section">
+    <div class="tile small">
+    <img class="live-img" src="images/portfolio/item8_sq.png" alt="Project Seven" />
+    </div>
+</a>
+	<!-- Lightbox Article Preview -->
+    <div class="tile-pre">
+    <article id="portfolio-8">
+    <div class="portfolio-img">
+    <img class="tile-pre-img" src="images/portfolio/item8.png" alt="Project Eight" />
+    </div>
+    </article>
+    </div>
+    
+<!-- END TILE CONTENT -->
+</section><!-- end #content-mos-->
+<section class="mainpage">
+<!-- BEGIN TOGGLE CONTENT -->
+<div class="toggle-button"></div>
+<!-- end .toggle-content -->
+<!-- END TOGGLE CONTENT -->
+
+</section><!-- end .main-page -->
+<!-- END MAIN PAGE CONTENT -->
+
+<!-- BEGIN FOOTER -->
+<footer class="clearfix">
+
+<small>&copy; Copyright &copy; 2016 CoL. All rights reserved. </small>
+</footer>
+<!-- END FOOTER -->
+
+</section><!-- end #container -->
+</div>
+
+</body>
+</html>
